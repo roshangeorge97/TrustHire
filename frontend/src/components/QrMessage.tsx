edf5f8eb843ad250f198e15f2592eaa7a78906ae
amanzrx4 <amansagar262@gmail.com>
2023-04-03 16:30:19 +0530

    redirect message

--- frontend/src/components/QrMessage.tsx
+++ frontend/src/components/QrMessage.tsx
@@ -3,18 +3,32 @@ import Arrow from './static/Svg/Arrow'
 
 const QrMessage = ({ appUrl }: { appUrl: string }) => {
 	return (
-		<a target="_blank" rel="noreferrer" href={appUrl}>
-			<div className="flex items-center lg:justify-start justify-center mb-10 gap  lg:gap-10">
-				<div className="p-2 bg-white rounded-2xl">
-					<QRCodeSVG className="w-54 h-54" value={appUrl} size={250} />
-				</div>
+		<div>
+			<div className="mb-4 block lg:hidden">
+				<a
+					className="text-blue-800 text-lg underline"
+					target="_blank"
+					rel="noreferrer"
+					href={appUrl}
+				>
+					Click here to open on Reclaim Wallet app
+				</a>
 
-				<Arrow className="hidden lg:block" />
+				<h3 className="text-yellow mt-2">OR</h3>
 			</div>
-			<h3 className="px-20 lg:px-0 lg:pr-20 text-2xl text-white font-Agrandir">
-				<span className="text-yellow">Scan Qr</span> to download the app
-			</h3>
-		</a>
+			<div>
+				<div className="flex items-center lg:justify-start justify-center mb-10 gap  lg:gap-10">
+					<div className="p-2 bg-white rounded-2xl">
+						<QRCodeSVG className="w-54 h-54" value={appUrl} size={250} />
+					</div>
+
+					<Arrow className="hidden lg:block" />
+				</div>
+				<h3 className="px-20 lg:px-0 lg:pr-20 text-2xl text-white font-Agrandir">
+					<span className="text-yellow">Scan Qr</span> to download the app
+				</h3>
+			</div>
+		</div>
 	)
 }
 
